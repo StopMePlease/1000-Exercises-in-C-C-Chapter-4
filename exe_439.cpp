c6590b74dd03f070f13c4ebba9d55372fc4c7913
Duc Do <dotrananhduc@gmail.com>
2016-08-29 23:26:55 +0700

    exe_439

---
+++ exe_439.cpp
@@ -0,0 +1,60 @@
+﻿/*
+	Exe_439: Tính tổng các phần tử thuộc ma trận tam giác dưới(không tính đường chéo) trong ma trận vuông các số thực
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int m, int n)
+{
+	for(int i = 0; i < m; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+
+void Result(int a[][100], int m, int n)
+{	
+	int sum = 0;
+	for(int i = 1; i < n; i++)
+	{
+		for(int j = 0; j < i; j++)
+		{
+			sum += a[i][j];
+		}
+	}
+	cout << sum;
+}
+
+int main()
+{
+	int a[100][100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+
+	Input(a, n, n);
+	Output(a, n, n);
+	Result(a, n, n);
+	
+	return 0;
+}
