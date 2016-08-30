74b2dd365406a55a73363abc5ee65487774102b9
Duc Do <dotrananhduc@gmail.com>
2016-08-30 11:45:03 +0700

    exe_440

---
+++ exe_440.cpp
@@ -0,0 +1,57 @@
+﻿/*
+	Exe_440: Tính tổng phần tử trên đường chéo chính
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+
+void Result(int a[][100], int n)
+{	
+	int sum = 0;
+	for(int i = 0; i < n; i++)
+	{
+		sum += a[i][i];
+	}
+	cout << sum;
+}
+
+int main()
+{
+	int a[100][100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	
+	return 0;
+}
