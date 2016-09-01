4ddc58370faaa7c09525471cd0b6a29280cb8f0d
Duc Do <dotrananhduc@gmail.com>
2016-09-01 14:30:52 +0700

    exe_444

---
+++ exe_444.cpp
@@ -0,0 +1,63 @@
+﻿/*
+	Exe_444: Tìm giá trị nhỏ nhất trong ma trận tam giác dưới
+*/
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Input(int a[][100], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "]: ";
+			cin >> a[i][j];
+		}
+	}
+}
+
+void Output(int a[][100], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			cout << setw(4) << a[i][j] ;
+		}
+		cout << endl;
+	}
+}
+
+
+void Result(int a[][100], int n)
+{	
+	int min = a[1][0];
+	for(int i = 1; i < n; i++)
+	{
+		for(int j = 0; j < i; j++)
+		{
+			if(a[i][j] < min)
+			{
+				min = a[i][j];
+			}
+		}
+	}
+	cout << min;
+}
+
+int main()
+{
+	int a[100][100];
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+
+	Input(a, n);
+	Output(a, n);
+	Result(a, n);
+	
+	return 0;
+}
